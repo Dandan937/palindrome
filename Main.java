4dd0bf0f9306f4e82e13ce43255d08a417cdffb7
Daniel Liu <35547415+Dandan937@users.noreply.github.com>
2022-10-22 01:39:07 +0000

    reorganized the structure of the program

--- Main.java
+++ Main.java
@@ -6,6 +6,7 @@ C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
+// import classes
 import java.util.Scanner;
 import java.lang.Math;
 import java.util.Arrays;
@@ -18,26 +19,33 @@ public class Main
 		System.out.println("Hello World");
 		// initialize objects
     File puzzle = new File("PalindromePuzzle.txt");
-    Scanner input2 = new Scanner(puzzle);
+    Scanner input = new Scanner(puzzle);
     // File updatedRecords = new File("updatedRecords.txt");
     // PrintWriter output = new PrintWriter(updatedRecords);
 
-    // find how many rows are in the puzzle
+    // variables to store how many lines there are in the puzzle and how long the lines are
     int lineCount = 0;
     int lineLength = 0;
-    while (input2.hasNext()) {
-      lineCount = lineCount + 1;
-      lineLength = input2.nextLine().length();
-    }
-    // restart the scanner stream
-    input2.close();
-    Scanner input = new Scanner(puzzle);
-
+    
     // if string contains 1 element, string[5] is an array and contains 5 elements, then string[5][10] should be an array that contains 10 five element arrays, right?... wrong. this thinking perceives the rightmost [] as how many of the type of array on the left there are, but this is wrong. it is actually just the leftmost [] is the greatest array and going rightwards the other [] describe the "nested" arrays, with the rightmost [] being the array that has only regular elements
-    String[][] board = new String[lineCount][lineLength];
-    for (int line = 0; line < lineLength; line++) {
-      System.out.println(line);
+    char[][] board;
+    while (input.hasNext()) {
+      String inputStr = input.nextLine();
+      lineLength = inputStr.length();
+      String str1 = inputStr.replaceAll(" ", "");
+      for (int element = 0; element < str1.length(); element++) {
+        
+      }
     }
+
+    // System.out.println(Arrays.toString(board[]));
+
+    // int ar[] = {2, 2, 1, 8, 3, 2, 2, 4, 2};
+  
+    // // Fill from index 1 to index 4.
+    // Arrays.fill(ar, 1, 5, 10);
+ 
+    // System.out.println(Arrays.toString(ar));
     input.close();
 	}
 }
